4c939b24c69daa6a9d2b327d8694238bd9abd1da
Chris Powers <chris.powers@berkeley.edu>
2019-03-09 13:14:53 -0800

    fix style

--- app/src/js/dev/gpu_test.js
+++ app/src/js/dev/gpu_test.js
@@ -3,8 +3,8 @@ import {sprintf} from 'sprintf-js';
 window.onload = function() {
     window.imageIndex = 0;
     updateImage();
-    let nextButton = document.getElementById("next");
-    let labelButton = document.getElementById("label");
+    let nextButton = document.getElementById('next');
+    let labelButton = document.getElementById('label');
     if (nextButton) {
         nextButton.onclick = function() {
             updateImage();
@@ -18,10 +18,11 @@ window.onload = function() {
 };
 /**
  * Gets sample URLs for images numbered from 51 to 99
+ * @return {string} url - Current sample url
  */
 function getNextSampleUrl() {
     let trueIndex = (window.imageIndex % 49) + 51;
-    let url = sprintf("https://s3-us-west-2.amazonaws.com/scalabel-public/demo/frames/intersection-00000%d.jpg", trueIndex);
+    let url = sprintf('https://s3-us-west-2.amazonaws.com/scalabel-public/demo/frames/intersection-00000%d.jpg', trueIndex);
     return url;
 }
 
@@ -31,15 +32,16 @@ function getNextSampleUrl() {
 function updateImage() {
     let image = new Image(1280, 720);
     let url = getNextSampleUrl();
-    image.onload=function(){
-        console.log("loaded");
-        let imageCanvas = document.getElementById('image-canvas');
-        let imageCtx = imageCanvas.getContext('2d');
+    image.onload=function() {
+        let canvasElement = (document.getElementById('image-canvas'): any);
+        let canvas = (canvasElement: HTMLCanvasElement);
+
+        let imageCtx = canvas.getContext('2d');
         imageCtx.clearRect(0, 0, 1280, 720);
         imageCtx.drawImage(image, 0, 0);
         imageCtx.restore();
         window.imageIndex += 1;
-    }
+    };
     image.src = url;
 }
 

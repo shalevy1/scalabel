51b1b6035739ce909669bb735f1d1649b30ed78c
Gary Chen <gary@datu.ai>
2019-10-10 11:02:55 -0700

    Add test for moving axis aligned 3d box along z axis

--- app/src/test/drawable/label3d_list.test.ts
+++ app/src/test/drawable/label3d_list.test.ts
@@ -1,7 +1,7 @@
 import _ from 'lodash'
 // import * as THREE from 'three'
 import * as action from '../../js/action/common'
-import { moveCameraAndTarget } from '../../js/action/point_cloud'
+import { moveCameraAndTarget, moveCamera } from '../../js/action/point_cloud'
 import Session from '../../js/common/session'
 import { initStore } from '../../js/common/session_init'
 import { Label3DList } from '../../js/drawable/3d/label3d_list'
@@ -9,6 +9,8 @@ import { getCurrentPointCloudViewerConfig, getShape } from '../../js/functional/
 import { CubeType, Vector3Type } from '../../js/functional/types'
 import { Vector3D } from '../../js/math/vector3d'
 import { testJson } from '../test_point_cloud_objects'
+import * as THREE from 'three'
+import { updateThreeCameraAndRenderer } from '../../js/view_config/point_cloud'
 
 /**
  * Check equality between two Vector3Type objects
@@ -75,4 +77,87 @@ test('Add 3d bbox', () => {
     expectVector3TypesClose(cube.size, { x: 1, y: 1, z: 1 })
     expect(cube.anchorIndex).toEqual(0)
   }
+})
+
+test('Move 3d bbox along axis', () => {
+  Session.devMode = false
+  initStore(testJson)
+  const itemIndex = 0
+  Session.dispatch(action.goToItem(itemIndex))
+
+  const camera = new THREE.PerspectiveCamera(45, 1, 0.1, 1000)
+  camera.aspect = 1
+
+  const label3dList = new Label3DList()
+  Session.subscribe(() => {
+    label3dList.updateState(
+      Session.getState(),
+      Session.getState().user.select.item
+    )
+  })
+
+  let state = Session.getState()
+
+  Session.dispatch(moveCameraAndTarget(
+    new Vector3D(), new Vector3D()
+  ))
+
+  const spaceEvent = new KeyboardEvent('keydown', { key: ' ' })
+  label3dList.onKeyDown(spaceEvent)
+
+  state = Session.getState()
+  expect(_.size(state.task.items[0].labels)).toEqual(1)
+
+  const labelId = Number(Object.keys(state.task.items[0].labels)[0])
+  Session.dispatch(action.selectLabel(labelId))
+
+  const tEvent = new KeyboardEvent('keydown', { key: 't' })
+  label3dList.onKeyDown(tEvent)
+
+  const position = new Vector3D()
+  position[1] = 10
+  Session.dispatch(moveCamera(
+    position
+  ))
+
+  state = Session.getState()
+  const viewerConfig = getCurrentPointCloudViewerConfig(state)
+  updateThreeCameraAndRenderer(viewerConfig, camera)
+  camera.updateMatrixWorld(true)
+
+  const raycastableShapes = label3dList.getRaycastableShapes()
+
+  const raycaster = new THREE.Raycaster()
+  raycaster.near = 1.0
+  raycaster.far = 100.0
+  raycaster.linePrecision = 0.02
+
+  raycaster.setFromCamera(new THREE.Vector2(0, 0.1), camera)
+  let intersections = 
+    raycaster.intersectObjects(raycastableShapes as unknown as THREE.Object3D[])
+  expect(intersections.length).toBeGreaterThan(0)
+
+  label3dList.onMouseMove(0, 0.1, camera, intersections[0])
+  label3dList.onMouseDown(0, 0.1, camera)
+  label3dList.onMouseMove(0, 0.5, camera)
+  label3dList.onMouseUp()
+
+  state = Session.getState()
+  let cube = getShape(state, 0, 0, 0) as CubeType
+
+  expect((new Vector3D()).fromObject(cube.center)[2]).toBeGreaterThan(0)
+
+  raycaster.setFromCamera(new THREE.Vector2(0, 0.5), camera)
+  intersections =
+    raycaster.intersectObjects(raycastableShapes as unknown as THREE.Object3D[])
+  expect(intersections.length).toBeGreaterThan(0)
+
+  label3dList.onMouseMove(0, 0.5, camera, intersections[0])
+  label3dList.onMouseDown(0, 0.5, camera)
+  label3dList.onMouseMove(0, 0.1, camera)
+  label3dList.onMouseUp()
+
+  state = Session.getState()
+  cube = getShape(state, 0, 0, 0) as CubeType
+  expect((new Vector3D()).fromObject(cube.center)[2]).toBeCloseTo(0)
 })
